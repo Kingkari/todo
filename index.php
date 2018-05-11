dbb0e9d23d1d0cf42f57f5532522085747234a3d
KingKari <sta22@njit.edu>
2018-05-10 22:20:57 -0400

    starting files-1

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
+<!Doctype html>
+<?php
+	require_once('tddb.php');
+	require_once('users_db.php');
+	require_once('user.php');
+	$users = UsersDB::getUsers();
+?>
 <html>
-</html>
+	<head>
+		<title>Todo List</title>
+		<link rel="stylesheet" href="main.css">
+	</head>
+	<body>
+        <?php echo "Hello " . 
+		<table class="usertable">
+			<tr>
+				<th>ID</th>
+				<th>email</th>
+				<th>fname</th>
+				<th>lname</th>
+				<th>phone</th>
+				<th>birthdate</th>
+				<th>gender</th>
+				<th>password</th>
+			</tr>	
+			<?php foreach ($users as $user) : ?>
+				<?php echo $user->displayUserRow(); ?>
+			<?php endforeach; ?>		
+		</table>
+	</body>		
+</html> 
