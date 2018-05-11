b324b9d44d47ed804970b38f29f3ff6185de25cb
KingKari <sta22@njit.edu>
2018-05-10 22:30:10 -0400

    naming changes + additions

--- control/signup.php
+++ control/signup.php
@@ -10,6 +10,14 @@
 	if ($conn->connect_error) {
     	die("Connection failed: " . $conn->connect_error);
     } 
+    $firstn = $_POST['firstn'];
+	$lastn = $_POST['lastn'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$gender = $_POST['gender'];
+    $bday = $_POST['birthday'];
     
+    $sql = "INSERT INTO userinfo (ID, firstn, lastn, email, phone, gender, birthday)
+	VALUES ('', '$firstn', '$lastn', '$email','$phone', '$gender', '$bday')";
     <form action="" method="post"></form>
     ?>
